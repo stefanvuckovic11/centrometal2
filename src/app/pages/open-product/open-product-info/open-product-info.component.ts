8bff20c37106027b1841c2d6a44f044790510894
Stefan Vuckovic <stefan.vuckovic@synergysuite.com>
2025-06-11 09:27:59 +0200

    fixed

--- src/app/pages/open-product/open-product-info/open-product-info.component.ts
+++ src/app/pages/open-product/open-product-info/open-product-info.component.ts
@@ -8,11 +8,11 @@ import { ProductInterface } from '../../../interfaces/product.interface';
   standalone: false
 })
 export class OpenProductInfoComponent implements OnChanges {
-  @Input() public product!: ProductInterface;
+  @Input() public product: ProductInterface | null = null;
   public quantity: number = 1;
 
   public get specEntries(): Array<{ label: string; value: string }> {
-    if (!this.product.specifications) {
+    if (!this.product?.specifications) {
       return [];
     }
     return Object.entries(this.product.specifications).map(
@@ -21,5 +21,8 @@ export class OpenProductInfoComponent implements OnChanges {
   }
 
   public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['product']) {
+      this.quantity = 1;
+    }
   }
 }
